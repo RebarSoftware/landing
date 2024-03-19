9647935635ac01e6e213190e3ae3ceed6c9f6643
fantomă <mrfakie789@gmail.com>
2024-03-19 00:50:09 -0400

    deck

---
+++ app/deck/page.tsx
@@ -0,0 +1,11 @@
+import { BaseLayout } from 'components/base';
+
+export default function Deck() {
+  return (
+    <BaseLayout>
+      <div className="h-screen w-screen flex items-center justify-center flex-col gap-6 pt-[56.25%]">
+      <iframe className="absolute top-0 left-0 bottom-0 right-0 w-full h-full" src="https://onedrive.live.com/embed?resid=D2E9AA3742F23AE7%21109&amp;authkey=!AK9lvJpmIgY1Eek&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=0" frameBorder="0" />
+      </div>
+    </BaseLayout>
+  );
+};
